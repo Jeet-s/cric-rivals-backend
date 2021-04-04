e8c4487ba43ae0effe7a2f2eedfc2b4914acec78
jeet-s <jagjeet@simplifiedautomation.ca>
2021-04-04 13:39:39 +0530

    added cors headers

--- src/app.js
+++ src/app.js
@@ -1,23 +1,34 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const passport = require('passport');
-var cors = require('cors');
-const http = require('http');
+const express = require("express");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+var cors = require("cors");
+const http = require("http");
 
-const authRoutes = require('./routes/auth');
-const socketService = require('./services/socketio');
+const authRoutes = require("./routes/auth");
+const socketService = require("./services/socketio");
 
-require('./db/mongoose');
-require('./services/passport');
+require("./db/mongoose");
+require("./services/passport");
 
 const app = express();
 
-socketService(http.createServer(app))
+socketService(http.createServer(app));
 
 app.use(cors());
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  );
+  next();
+});
+
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(passport.initialize());
 app.use(passport.session());
