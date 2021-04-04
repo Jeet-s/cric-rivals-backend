0172d479fbc5ae53f27cd330c7ee233cc683f890
jeet-s <jagjeet@simplifiedautomation.ca>
2021-04-04 14:09:54 +0530

    fixed view error

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ const app = require("./app");
 
 const port = process.env.PORT || 3000;
 
-app.get("*", (req, res) => {
-  res.render("error", {
-    title: "404",
-    errorMessage: "Page not found.",
-  });
-});
+// app.get("*", (req, res) => {
+//   res.render("error", {
+//     title: "404",
+//     errorMessage: "Page not found.",
+//   });
+// });
 
 console.log("port=>", port);
 
