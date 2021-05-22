4b189a7c5f2be2100858268fa3649b51ff39efa2
jeet-s <jagjeet@simplifiedautomation.ca>
2021-05-22 16:15:48 +0530

    added console logs

--- src/services/socketio.js
+++ src/services/socketio.js
@@ -17,23 +17,17 @@ module.exports = function socket(server) {
     let userRoom = users.find(
       (x) => x.userId == userId || x.opponentId == userId
     );
-    // if (userRoom) {
-    //   console.log(
-    //     "Joined room again",
-    //     io.sockets.adapter.rooms.get(userRoom.roomId)
-    //   );
-    //   socket.join(userRoom.roomId);
-    //   userRoom.socketId = socket.id;
-    // }
+    if (userRoom) {
+      socket.join(userRoom.roomId);
+      userRoom.socketId = socket.id;
+    }
 
     console.log("user connected");
-    socket.on("disconnect", (data) => {
-      console.log("user disconnected", data);
+    socket.on("disconnect", () => {
+      console.log("user disconnected");
     });
 
     socket.on("create-game", (data) => {
-      console.log("Before Create ============>>>>>>", users);
-
       if (
         !io.sockets.adapter.rooms.has(data.roomId) &&
         users.findIndex((x) => x.userId == userId && x.roomId == data.roomId) ==
@@ -42,9 +36,6 @@ module.exports = function socket(server) {
         console.log("create", data.roomId);
         socket.join(data.roomId);
         users.push({ socketId: socket.id, ...data });
-        console.log("After Create ============>>>>>>", users);
-
-        console.log("join GAME DATA=>", { socketId: socket.id, ...data });
       } else {
         console.log("error create", data.roomId);
         socket.emit("error", "Room Id not available");
@@ -52,19 +43,6 @@ module.exports = function socket(server) {
     });
 
     socket.on("join-game", (data) => {
-      console.log("Before Join ============>>>>>>", users);
-      console.log(
-        io.sockets.adapter.rooms.has(data.roomId),
-        io.sockets.adapter.rooms.get(data.roomId).size == 1,
-        users.findIndex(
-          (x) => x.opponentId == userId && x.roomId == data.roomId
-        ) == -1
-      );
-      console.log(
-        users.findIndex(
-          (x) => x.opponentId == userId && x.roomId == data.roomId
-        )
-      );
       if (
         io.sockets.adapter.rooms.has(data.roomId) &&
         io.sockets.adapter.rooms.get(data.roomId).size == 1 &&
@@ -78,18 +56,11 @@ module.exports = function socket(server) {
         let user = { socketId: socket.id, ...data };
         users.push(user);
 
-        console.log("join GAME DATA=>", user);
-
         let startData = {
           roomId: data.roomId,
-          userId: users.find(
-            (x) => x.userId == userId && x.roomId == data.roomId
-          ),
+          userId: users.find((x) => x.roomId == data.roomId).userId,
           opponentId: data.opponentId,
         };
-
-        console.log("START GAME DATA=>", startData);
-        console.log("After Join ============>>>>>>", users);
 
         io.to(data.roomId).emit("start-game", startData);
       } else {
