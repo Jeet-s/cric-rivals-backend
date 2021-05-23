f99dd5b8be22ca60f37aa0ed5a51a50840020ba6
jeet-s <jagjeet@simplifiedautomation.ca>
2021-05-23 16:13:10 +0530

    cors error

--- src/services/socketio.js
+++ src/services/socketio.js
@@ -2,7 +2,7 @@ const socketio = require("socket.io");
 module.exports = function socket(server) {
   let io = socketio(server, {
     cors: {
-      origin: "http://localhost",
+      origin: "*",
       methods: ["GET", "POST"],
       allowedHeaders: ["my-custom-header"],
       credentials: true,
